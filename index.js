19444b5d1d3cd7cad55c5405a131c8fae372846e
Elijah Prince <elijahprince73@gmail.com>
2018-04-29 15:25:33 -0700

    Access tokens

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy
+const keys = require('./config/keys');
 
 const app = express()
-// client ID: 656046300885-oc86qdp6s91g0b4laept87lldkksueak.apps.googleusercontent.com
 
-// client secret:
-g2QYlqVgtwiq8iiumtaNdG7m
+passport.use(new GoogleStrategy({
+	clientID: keys.googleClientID,
+	clientSecret: keys.googleClientSecret,
+	callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+	console.log('Access Token', accessToken);
+	console.log('Refresh token', refreshToken);
+	console.log('profile', JSON.stringify(profile, undefined, 2));
+}))
 
+app.get('/auth/google', passport.authenticate('google', {
+	scope: ['profile', 'email']
+}))
 
-
-passport.use(new GoogleStrategy())
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 const PORT = process.env.PORT || 3000
 
