12d60f77e6e58938de0b8531934b787e8a6abfc1
Elijah Prince <elijahprince73@gmail.com>
2018-06-17 21:34:22 -0700

    Created recipients subdocument

--- models/Survey.js
+++ models/Survey.js
@@ -2,13 +2,23 @@ const mongoose = require('mongoose');
 const {
   Schema
 } = mongoose
+const RecipientSchema = require('./Recipient');
 
 const surveySchema = new Schema({
   title: String,
   body: String,
   subject: String,
-  // This tells mongoose that the recipients will be an array containing strings
-  recipients: [String]
+  // This tells mongoose that the recipients will be a subdocument with an
+  // array of recipients
+  recipients: [RecipientSchema],
+  yes: {
+    type: Number,
+    default: 0
+  },
+  no: {
+    type: Number,
+    default: 0
+  }
 })
 
 mongoose.model('surveys', surveySchema)
