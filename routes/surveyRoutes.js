0b36cff73322e085e414ae8f7a489e60f56a88bc
Elijah Prince <elijahprince73@gmail.com>
2018-06-24 16:32:42 -0700

    Mailer constructor

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
-
-const Survey = mongoose.model('surveys')
+const Mailer = require('../services/Mailer');
+const Survey = mongoose.model('surveys');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 module.exports = app => {
   app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
@@ -24,5 +25,7 @@ module.exports = app => {
       _user: req.user.id,
       dateSent: Date.now(),
     })
+    // Sends email
+    const mailer = new Mailer(survey, surveyTemplate(survey))
   })
 }
