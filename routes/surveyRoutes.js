a98f785b5a19a902b1890ad634676960da0d5636
Elijah Prince <elijahprince73@gmail.com>
2018-08-14 22:02:37 -0700

    id surveyId, email, and choice back from survey sent

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const Path = require('path-parser').default;
+const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -11,9 +14,25 @@ module.exports = app => {
 	});
 
 	app.post('/api/surveys/webhooks', (req, res) => {
-		console.log(req.body);
+		const events = req.body.map(({ email, url }) => {
+			const pathname = new URL(url).pathname;
+			const p = new Path('/api/surveys/:surveyId/:choice');
+			const match = p.test(pathname);
+			if (match) {
+				return {
+					email,
+					surveyId: match.surveyId,
+					choice: match.choice
+				};
+			}
+		});
+		// Removes any undefined
+		const compactEvents = _.compact(events);
+		// Makes sure that each one is unique
+		const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
 		res.send({});
 	});
+
 	app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
 		const { title, body, subject, recipients } = req.body;
 
