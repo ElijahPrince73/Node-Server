6705059f615f879390c51c733bb084cecb8163c9
Elijah Prince <elijahprince73@gmail.com>
2018-08-21 22:40:30 -0700

    fix for heroku

--- client/src/components/surveys/SurveyForms.js
+++ client/src/components/surveys/SurveyForms.js
@@ -3,7 +3,7 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import { Link } from 'react-router-dom';
-import SurveyField from './SurveyField';
+import SurveyField from './SurveyFields';
 import validateEmails from '../../utils/validateEmails';
 import formFields from './formFields';
 
